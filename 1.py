3981e8dbc697279a5975b1738213924e8d83a557
Irina <andrey_k.90@mail.ru>
2022-09-08 00:51:59 +0300

    вторая задача

--- 1.py
+++ 1.py
@@ -6,10 +6,25 @@
 #- 1 -> нет
 
 
-D = int(input('Введите день недели: '))
-if D == 5:
-    print('Выходной')
-elif D == 6:
-    print('Выходной')
-else:
-    print('будний')
+
+#D = int(input('Введите день недели: '))
+#if D == 5:
+ #   print('Выходной')
+#elif D == 6:
+    #print('Выходной')
+#else:
+  #  print('будний')
+    
+    
+    
+#Напишите программу для проверки истинности утверждения
+# ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+#a = 0
+#b = 1
+#for x in range (a, b):
+ #   for y in range (a, b):
+  #      for z in range (a, b):
+   #         print ((not x or not y or not z) == (not x and not y and not z))
+
+
