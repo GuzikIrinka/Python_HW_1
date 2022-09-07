273b9ddba55799f27ecb3b9f814ef9e0b9068118
Irina <andrey_k.90@mail.ru>
2022-09-08 00:52:37 +0300

    вторая задача

--- 1.py
+++ 1.py
@@ -28,3 +28,36 @@
    #         print ((not x or not y or not z) == (not x and not y and not z))
 
 
+#Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0
+# и выдаёт номер четверти плоскости, в которой находится эта точка (или на какой оси она находится).
+
+#Пример:
+#- x=34; y=-30 -> 4
+#- x=2; y=4-> 1
+#- x=-34; y=-30 -> 3
+
+#x_point = int (input ('Введите координату x: ')) 
+#y_point = int (input ('Введите координату y: ')) 
+#if ((x_point == 0) or (y_point == 0)):
+ #   print ('ошибка!')
+#elif (x_point < 0 and y_point > 0):
+ #   print ('1 четверть')
+#elif (x_point > 0 and y_point > 0):
+ #   print ('2 четверть')
+#elif (x_point > 0 and y_point < 0):
+ #   print ('3 четверть')
+#elif (x_point < 0 and y_point < 0):
+ #   print ('4 четверть')
+ 
+x_point = int (input ('Введите координату x: ')) 
+y_point = int (input ('Введите координату y: ')) 
+if ((x_point == 0) or (y_point == 0)):
+    print ('ошибка!')
+elif (x_point < 0 and y_point > 0):
+    print ('1 четверть')
+elif (x_point > 0 and y_point > 0):
+    print ('2 четверть')
+elif (x_point > 0 and y_point < 0):
+    print ('3 четверть')
+elif (x_point < 0 and y_point < 0):
+    print ('4 четверть')
